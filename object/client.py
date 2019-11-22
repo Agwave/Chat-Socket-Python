c032e8c2bf306896a5a8beab4c82c4134bd5ca7d
Agwave <agwave@foxmail.com>
2019-11-23 07:21:38 +0800

    客户端初步完成接受message功能 --191123

--- object/client.py
+++ object/client.py
@@ -1,3 +1,5 @@
+import io
+import json
 import socket
 import selectors
 import types
@@ -66,18 +68,41 @@ class Client(object):
         self._server_host = server_host
         self._server_port = server_port
         self.socket = None
+        self.t = None
         self._start_connections()
 
     def _start_connections(self):
         server_addr = (self._server_host, self._server_port)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(server_addr)
-        threading.Thread(target=self.read_from_server).start()
+        self.t = threading.Thread(target=self.read_from_server, daemon=True)
+        self.t.start()
 
     def read_from_server(self):
         while True:
-            recv = self.socket.recv(2048).decode("utf-8")
-            print(recv)
+            recv = self.socket.recv(2048)
+            self._process_recv(recv)
+
+    def _process_recv(self, data_recv):
+        data_dict = self._json_decode(data_recv)
+        if data_recv is not None:
+            action = data_dict.get("action")
+            if action == "transmit":
+                value = data_dict.get("value")
+                from_id = data_dict.get("from_id")
+                print("recv: {} from {}".format(value, from_id))
+            elif action == "login" or action == "out":
+                pass
+            else:
+                print(f'Error: invalid action "{action}".')
+
+    def _json_decode(self, json_bytes, encoding="utf-8"):
+        tiow = io.TextIOWrapper(
+            io.BytesIO(json_bytes), encoding=encoding, newline=""
+        )
+        obj = json.load(tiow)
+        tiow.close()
+        return obj
 
     def sign_out(self):
         self.socket.close()
